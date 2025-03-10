c76ab3a7ff1c572ebb6507a6d4c76bb69abd43c2
Nikola Maksimovic <nikola.maksimovic@orioninc.com>
2025-03-10 12:23:39 +0100

    Created starter components, implemented the initial routing; Used Lazy Loading feature.

---
+++ Client/src/app/current-announcements/current-announcements.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-announcements',
+  imports: [],
+  templateUrl: './current-announcements.component.html',
+  styleUrl: './current-announcements.component.css'
+})
+export class CurrentAnnouncementsComponent {
+
+}
---
+++ Client/src/app/new-announcement/new-announcement.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-announcement',
+  imports: [],
+  templateUrl: './new-announcement.component.html',
+  styleUrl: './new-announcement.component.css'
+})
+export class NewAnnouncementComponent {
+
+}
